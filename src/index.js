c10a97df662a233b5bd49c6780623fe3bfbc22e2
Jaz11 <jahzell111@gmail.com>
2018-05-30 08:33:58 -0500

    Aplicndo la función encode

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-//Calling all my elements
 //const container = document.getElementsByClassName("container");
 
 //const firstButton = document.getElementById("first-button");
 
-//Llamo mis elementos, y le doy funcionalidad a mi botón
+//Llamo mis elementos, y le doy funcionalidad al primer botón de envío
 document.getElementById("second-button").addEventListener("click", () => { debugger
     //Variable para la frase secreta, lo convierto a mayúscula para que lea el ascii code
     let string = document.getElementById("firstphrase").value.toUpperCase();
     //Variable para el número de desplazamiento, primero lo convierto a number
     let offset = parseInt(document.getElementById("parameter").value);
+    //Llamamos a la función encode e imprimimos
     let encrypt = window.cipher.encode(offset, string);
     document.getElementById("result").innerHTML = encrypt.toLowerCase();    
-    
-}
+    }
 )
 
+//Llamo mis elementos, y le doy funcionalidad al segundo botón de envío
+document.getElementById("third-button").addEventListener("click", () => { debugger
+    //Variable para la frase secreta, lo convierto a mayúscula para que lea el ascii code
+    let string = document.getElementById("secondphrase").value.toUpperCase();
+    //Variable para el número de desplazamiento, primero lo convierto a number
+    let offset = parseInt(document.getElementById("parameter").value);
+    //Llamamos a la función encode e imprimimos
+    let encrypt = window.cipher.encode(offset, string);
+    document.getElementById("result").innerHTML = encrypt.toLowerCase();    
+    }
+)
 
