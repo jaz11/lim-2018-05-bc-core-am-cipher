5fcd6224297734c03e7452262817c0734ef770fe
jaz11 <jahzell111@gmail.com>
2018-05-30 20:02:37 -0500

    bajando cambios a mi local

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,49 @@
 window.cipher = {
-  // ... 
-};
+    encode: (offset, string) =>
+       { 
+        let asciiCode;
+        let encrypt = "";
+       
+        for (let a = 0; a < string.length; a++) {
+             asciiCode = string[a].charCodeAt();
+             if (asciiCode >= 65 && asciiCode <= 90) {
+                encrypt = encrypt + String.fromCharCode((asciiCode - 65 + parseInt(offset)) % 26 + 65);
+            }
+             else {
+                encrypt = encrypt + string[a];
+            }
+        }
+        return encrypt;
+        },
+
+    decode: (offset, string) => 
+        { debugger
+         let asciiCode;
+         let decrypt = "";
+         
+         for (let a = 0; a < string.length; a++) {
+             asciiCode = string[a].charCodeAt();
+             if (asciiCode >= 65 && asciiCode <= 90) {
+                decrypt = decrypt + String.fromCharCode((asciiCode - 65 - parseInt(offset) + parseInt(offset)) % 26 + 65);
+              }
+             else {
+                decrypt = decrypt + string[a];
+                  }
+          } 
+            
+            return decrypt;
+        },
+        
+
+    createCipherWithOffset: (offset) => {
+
+        return {
+            encode: (string) => {
+                return cipher.encode(offset, firstphrase);
+            },
+            decode: (string) => {
+                return cipher.decode(offset, secondphrase)
+            }
+        }
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,29 @@
+//const container = document.getElementsByClassName("container");
+
+//const firstButton = document.getElementById("first-button");
+
+ //Llamo mis elementos, y le doy funcionalidad al primer botón de envío
+    document.getElementById("second-button").addEventListener("click", () => { 
+    //Variable para la frase secreta, lo convierto a mayúscula para que lea el ASCII code
+    let string = document.getElementById("firstphrase").value.toUpperCase();
+    //Variable para el número de desplazamiento, primero lo convierto en number
+    let offset = parseInt(document.getElementById("parameter").value);
+    //Llamando a mi función cifrar
+    let encrypt = cipher.encode(offset, string);
+    document.getElementById("result").innerHTML = encrypt.toLowerCase();    
+    }
+)
+
+//Llamo mis elementos, y le doy funcionalidad al segundo botón de envío
+    document.getElementById("third-button").addEventListener("click", () => {
+    //Variable para la frase cifrada, lo convierto a mayúscula para que lea el ASCII code
+    let string = document.getElementById("secondphrase").value.toUpperCase();
+    //Variable para el número de desplazamiento, lo convierto en number
+    let offset = parseInt(document.getElementById("parameter").value);
+    //Llamando a mi función descifrar
+    let decrypt = cipher.decode(offset, string);
+    document.getElementById("result2").innerHTML = decrypt.toLowerCase();    
+    }
+)
+
+
